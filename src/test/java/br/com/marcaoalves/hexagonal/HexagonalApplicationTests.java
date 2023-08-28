c0e51b2c62633f0573876c9d52a21bb825854287
Marcos Alves <eng.marcos.alves@gmail.com>
2023-08-28 12:45:55 -0300

    primeiro commit

---
+++ src/test/java/br/com/marcaoalves/hexagonal/HexagonalApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marcaoalves.hexagonal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexagonalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
