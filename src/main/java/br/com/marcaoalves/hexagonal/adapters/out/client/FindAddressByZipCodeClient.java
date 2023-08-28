8f13f07af8f9059692077b87d917d8963cbf88c5
Marcos Alves <eng.marcos.alves@gmail.com>
2023-08-28 13:55:05 -0300

    criando o client rest para busca de endereço por cep

---
+++ src/main/java/br/com/marcaoalves/hexagonal/adapters/out/client/FindAddressByZipCodeClient.java
@@ -0,0 +1,17 @@
+package br.com.marcaoalves.hexagonal.adapters.out.client;
+
+import br.com.marcaoalves.hexagonal.adapters.out.client.response.AddressResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(
+        name = "FindAddressByZipCodeClient",
+        url = "${marcaoalves.client.address.url}"
+)
+public interface FindAddressByZipCodeClient {
+
+    @GetMapping("/{zipCode}")
+    AddressResponse find(@PathVariable("zipCode") String zipCode);
+
+}
---
+++ src/main/java/br/com/marcaoalves/hexagonal/adapters/out/client/response/AddressResponse.java
@@ -0,0 +1,13 @@
+package br.com.marcaoalves.hexagonal.adapters.out.client.response;
+
+import lombok.Data;
+
+@Data
+public class AddressResponse {
+
+    private String street;
+
+    private String city;
+
+    private String state;
+}
